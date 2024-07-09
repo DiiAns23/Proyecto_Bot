4cc5822cd9701cb84faeeaf37d25b09ec33a9de8
Diego Andrés Obín Rosales <2742934200101@ingenieria.usac.edu.gt>
2024-07-09 17:49:13 -0600

    feat: Se agregaron respuestas automatizadas

--- backend/src/controllers/usuario.controller.js
+++ backend/src/controllers/usuario.controller.js
@@ -1,25 +1,7 @@
 const conexion = require('../middlewares/query');
 const enviarCorreo = require('../middlewares/mailer')
-//const tensorflow = require('@tensorflow/tfjs-node')
+const similitud = require('string-similarity');
 
-const cargar_modelo = async () => {
-    const modelo = await tensorflow.loadLayersModel();
-    return modelo
-}
-
-const procesar_pregunta = async (pregunta) => {
-    // Desestructurar la pregunta por palabras clave eje. 'Asignacion' , 'Fecha', 'DPI', 'Graduacion'
-    // Obtener esa cadena de tokens esenciales
-}
-
-const similitud_preguntas = async (pregunta, modelo) => {
-    const similitud = tensorflow.dot(pregunta, modelo);
-
-    const magnitud_1 = tensorflow.norm(pregunta);
-    const magnitud_2 = tensorflow.norm(modelo);
-
-    return similitud.div(magnitud_1.mul(magnitud_2));
-}
 
 const nueva_pregunta = async (req, res) => {
     const { nombre, apellido, correo, edad, pregunta } = req.body;
@@ -27,49 +9,40 @@ const nueva_pregunta = async (req, res) => {
     const query = `
     INSERT INTO Dudas (nombre, apellido, correo, edad, pregunta) 
         VALUES ('${nombre}', '${apellido}', '${correo}', '${edad}', '${pregunta}');`
-    
+        
     const result = await conexion.execute(query);
-
     if (result.error) {
         res.json({ error: result.error });
     }else{
-        const mensaje = `
-        <h1>DEPARTAMENTO DE MATEMATICA USAC</h1>
+        const query2 = `SELECT * FROM Respuestas;`
+        const result2 = await conexion.execute(query2);
+        const preguntas = result2.rows.map(respuesta => respuesta.pregunta);
 
-        <p>Hola ${nombre}, agradecemos que te comuniques con nosotros, tu pregunta
-        estara siendo procesada y se dara solucion lo mas rapido posible.
-        </p>
-
-        <br>
-
-        <p>Pregunta creada: ${pregunta}</p>
-
-        PAP - 2024
-        ADIOS
-        `
-        await enviarCorreo(correo, 'Duda del PAP', mensaje);
-        res.json({ message: 'Pregunta creada' });
-    }
-}
-
-const respuesta = async (req, res) => {
-    const { pregunta, respuesta, satisfactoria } = req.body;
-
-    if(satisfactoria){
-        const query = `
-        INSERT INTO Respuestas (pregunta, respuesta) 
-            VALUES ('${pregunta}', '${respuesta}');`
-        
-        const result = await conexion.execute(query);
-        if (result.error) {
-            res.json({ error: result.error });
-        }else{
-            res.json({ message: 'La respuesta fue satisfactoria' });
+        const mejorSimilitud = similitud.findBestMatch(pregunta, preguntas);
+        if(mejorSimilitud.bestMatch.rating > 0.6){
+            const mejorPregunta = result2.rows[mejorSimilitud.bestMatchIndex];
+            const respuesta = mejorPregunta.respuesta;
+            console.log('Esta es la respuesta sugerida:')
+            console.log(respuesta)
         }
-        console.log('La respuesta fue satisfactoria');
-    }else{
-        console.log('La respuesta no fue satisfactoria');
-        //Envio de los datos por correo
+
+        // const mensaje = `
+        // <h1>DEPARTAMENTO DE MATEMATICA USAC</h1>
+
+        // <p>Hola ${nombre}, agradecemos que te comuniques con nosotros, tu pregunta
+        // estará siendo procesada y se dará solución lo más rápido posible.
+        // </p>
+
+        // <br>
+
+        // <p>Pregunta creada: ${pregunta}</p>
+
+        // PAP - 2024
+        // ADIOS
+        // `
+        //await enviarCorreo(correo, 'Duda del PAP', mensaje);
+
+        res.json({ message: 'Pregunta creada' });
     }
 }
 
