6132614125ed4c3d568752b318ecc066c9a17db0
Diego Andrés Obín Rosales <2742934200101@ingenieria.usac.edu.gt>
2024-09-19 09:19:16 -0600

    feat: Cambios de visualizacion

---
+++ frontend/src/app/componets/resueltas/resueltas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resueltas',
+  standalone: true,
+  imports: [],
+  templateUrl: './resueltas.component.html',
+  styleUrl: './resueltas.component.scss'
+})
+export class ResueltasComponent {
+
+}
